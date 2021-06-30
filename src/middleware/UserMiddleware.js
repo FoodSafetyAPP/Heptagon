f23dfebf6a8147ef39315dbc5167e72f37cec186
ankushkatiyar <ankushkatiyar@cedcoss.com>
2021-06-30 23:28:36 +0530

    description color

--- src/middleware/UserMiddleware.js
+++ src/middleware/UserMiddleware.js
@@ -2,7 +2,7 @@ export const userLogin = (username, password) => {
   let user = JSON.parse(localStorage.getItem('user'));
   return new Promise((resolve, reject) => {
     if (user !== null && username === user.username && password === user.password) {
-      resolve("You Have Successfully Logged in to Heptagon !");
+      resolve("Logged in Successfully !");
     } else {
       reject('Username or password is incorrect !');
     }
