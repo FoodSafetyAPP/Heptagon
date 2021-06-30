f4ab6b25e47b9c6f895e77322f00bb04009d1a1d
ankushkatiyar <ankushkatiyar@cedcoss.com>
2021-06-30 20:47:22 +0530

    Add chart

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,6 +6,17 @@ import { records } from "../assets/data";
 import Chart from "./chart/Chart";
 
 class HomePage extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      typingTimeout: 0,
+      occurence: []
+    };
+  }
+
   componentDidMount() {
     const { loggedIn, updateList } = this.props;
     if (!loggedIn) {
@@ -15,40 +26,82 @@ class HomePage extends Component {
     }
   }
 
+  countOccurrences = (str, word) => {
+    let a = str.split(" ");
+    let count = 0;
+    for (let i = 0; i < a.length; i++) {
+      if (word === (a[i]))
+        count++;
+    }
+    return count;
+  }
+
+  handleChange(e) {
+    const { typingTimeout } = this.state;
+    const { dataList } = this.props;
+
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+
+    let descstr = "";
+    let countWords = [];
+    dataList.forEach(element => {
+      descstr = descstr + " " + element.description.toLowerCase();
+    });
+    descstr = descstr.trim();
+
+    this.setState({
+      typingTimeout: setTimeout(() => {
+        const arr = e.target.value.trim().split(" ");
+        arr.forEach((element) => {
+          const data = element.trim();
+          const count = this.countOccurrences(descstr, data);
+          countWords.push({ keyword: data, count });
+          this.setState({ occurence: countWords });
+        });
+      }, 1000),
+    });
+  }
+
   render() {
     const { userLogout, dataList } = this.props;
+    const { occurence } = this.state;
     return (
       <div className="container py-3">
         <header>
           <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
             <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-              <button onClick={userLogout} type="button" name="logout" className="btn btn-primary btn-sm btn-block">Logout</button>
+              <input onChange={this.handleChange} autoComplete="off" type="text" name="search" id="search" placeholder="search..." style={{ width: "400px" }} />
+              <button style={{ marginLeft: "10px" }} onClick={userLogout} type="button" name="logout" className="btn btn-primary btn-sm btn-block">Logout</button>
             </nav>
           </div>
         </header>
-        {dataList.length > 0 ?
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Description</th>
-                <th scope="col">Date</th>
-                <th scope="col">Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {dataList.map((data) => {
-                return (
-                  <tr key={data.id}>
-                    <td>{data.description}</td>
-                    <td>{data.date}</td>
-                    <td>{data.price}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-          : ""}
-        <Chart />
+        <div>
+          {dataList.length > 0 ?
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Description</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataList.map((data) => {
+                  return (
+                    <tr key={data.id}>
+                      <td>{data.description}</td>
+                      <td>{data.date}</td>
+                      <td>{data.price}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            : ""}
+          <Chart searchKeyword={occurence} />
+        </div>
       </div>
     )
   }
--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,32 +1,30 @@
 import React from "react";
+import uuid from 'react-uuid';
 import classess from "./chart.module.css";
 
 function Chart(props) {
+
+  const { searchKeyword } = props;
   return (
     <div className={classess.mwb_bar}>
       <table className={classess.q_graph}>
         <tbody>
-          <tr className={`${classess.qtr} ${classess.q1}`}>
-            <th scope='row' className={`${classess.qtr} ${classess.headData}`}>
-              Lorem
-            </th>
-            <td
-              className={`${classess.sent} ${classess.bar} ${classess.qtr}`}
-            >
-              <p className={classess.price}>
-              </p>
-            </td>
-          </tr>
+          {
+            searchKeyword.map((value) => {
+              return (
+                <tr className={`${classess.qtr} ${classess.q1}`} key={uuid()}>
+                  <th scope='row' className={`${classess.qtr} ${classess.headData}`}>
+                    {value.keyword}
+                  </th>
+                  <td className={`${classess.sent} ${classess.bar} ${classess.qtr}`} style={{ height: value.count * 10 }}></td>
+                </tr>
+              )
+            })
+          }
         </tbody>
       </table>
 
       <div className={classess.ticks}>
-        <div className={classess.tick}>
-          <p className={classess.priceLst}>25</p>
-        </div>
-        <div className={classess.tick}>
-          <p className={classess.priceLst}>20</p>
-        </div>
         <div className={classess.tick}>
           <p className={classess.priceLst}>15</p>
         </div>
