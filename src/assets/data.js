66904ad63ae76c58065db526bb979620d6a7f7fd
ankushkatiyar <ankushkatiyar@cedcoss.com>
2021-07-02 02:00:28 +0530

    Fix search

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,58 +1,36 @@
-export const records = [
-  {
-    "id": 1,
-    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-    "date": "2021-06-01",
-    "price": "0.3",
-    "matched": []
+export const records = [{
+  "id": 1,
+  "title": "Antwoord op vragen van de leden Kröger en Klaver over het COVID-19 virus",
+  "description": {
+    "desc1": "...Dit is een algemene richtlijn, vandaar dat er voor de omgang met corona een specifieke richtlijn is in de nabijheid van een corona- tiënt",
+    "desc2": "Vraag 11 Wat is de marktprijs voor het inkopen van een FFP1 mondkapje, hoeveel mondkapjes hiervan zijn,de oproep van de burgemeester van Rotterdam dat er geen",
+    "desc3": "Lorem Ipsum is"
   },
-  {
-    "id": 2,
-    "description": "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    "date": "2021-06-02",
-    "price": "0.2",
-    "matched": []
+  "date": "2021-06-01",
+  "score": "0.3",
+  "matched": []
+},
+{
+  "id": 2,
+  "title": "Lorem custom 2",
+  "description": {
+    "desc1": "Lorem Ipsum is simply dummy demo text of the printing and typesetting industry",
+    "desc2": "Lorem Ipsum is simply dummy data",
+    "desc3": "Lorem Ipsum"
   },
-  {
-    "id": 3,
-    "description": "Contrary to popular belief, Lorem Ipsum is not simply random text",
-    "date": "2021-06-03",
-    "price": "0.29",
-    "matched": []
+  "date": "2021-06-02",
+  "score": "1.3",
+  "matched": []
+},
+{
+  "id": 3,
+  "title": "Lorem title 3",
+  "description": {
+    "desc1": "Contrary to popular belief, Lorem Ipsum is not simply random text",
+    "desc2": "Contrary to popular to",
+    "desc3": "Lorem Ipsum simply"
   },
-  {
-    "id": 4,
-    "description": "There are many variations of passages of Lorem Ipsum available",
-    "date": "2021-06-04",
-    "price": "0.9",
-    "matched": []
-  },
-  {
-    "id": 5,
-    "description": "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested",
-    "date": "2021-06-05",
-    "price": "0.25",
-    "matched": []
-  },
-  {
-    "id": 6,
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    "date": "2021-06-06",
-    "price": "0.212",
-    "matched": []
-  },
-  {
-    "id": 7,
-    "description": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti",
-    "date": "2021-06-07",
-    "price": "0.6",
-    "matched": []
-  },
-  {
-    "id": 8,
-    "description": "These cases are perfectly simple and easy to distinguish",
-    "date": "2021-06-08",
-    "price": "0.77",
-    "matched": []
-  }
-];
+  "date": "2021-06-03",
+  "score": "5.3",
+  "matched": []
+}]
--- src/store/Actions/UserActions.js
+++ src/store/Actions/UserActions.js
@@ -1,35 +1,65 @@
 import { userConstants } from '../Constants/UserConstants';
-import { userLogin } from "../../middleware/UserMiddleware";
 import { alertError, alertClear, alertSuccess } from "./AlertActions";
 import { history } from "../../helpers/history";
+import { store } from "../Store";
+import axios from "axios";
+
+export const login_failure = () => {
+  store.dispatch({ type: userConstants.LOGIN_FAILURE });
+  store.dispatch(alertError("Username or password is incorrect !"));
+  setTimeout(() => {
+    store.dispatch(alertClear());
+  }, 1000);
+}
+
+export const logout_failure = () => {
+  localStorage.removeItem("access_token");
+  store.dispatch({ type: userConstants.LOGOUT });
+  history.replace('/login');
+}
 
 export const login = (username, password) => {
   return dispatch => {
-    userLogin(username, password)
-      .then(
-        msg => {
+    axios
+      .post('/login-user?email=' + username + '&password=' + password)
+      .then((res) => {
+        if (res.data.success === true) {
+          localStorage.setItem("access_token", res.data.authenticated_data);
           dispatch({ type: userConstants.LOGIN_SUCCESS });
-          dispatch(alertSuccess(msg));
+          dispatch(alertSuccess("Logged in Successfully !"));
           setTimeout(() => {
             dispatch(alertClear());
             history.replace('/');
           }, 1000);
-        },
-        error => {
-          dispatch({ type: userConstants.LOGIN_FAILURE });
-          dispatch(alertError(error));
-          setTimeout(() => {
-            dispatch(alertClear());
-          }, 1000);
+        } else {
+          login_failure();
         }
-      );
+      })
+      .catch(() => {
+        login_failure();
+      });
   };
 }
 
 export const logout = () => {
+  const headers = {
+    'Content-Type': 'application/json',
+    "authorization": `Bearer ${localStorage.getItem("access_token")}`
+  };
   return dispatch => {
-    dispatch({ type: userConstants.LOGOUT });
-    history.replace('/login');
+    axios
+      .get('/logout-user', { headers })
+      .then((res) => {
+        console.log(res);
+        if (res.data.success === true) {
+          logout_failure();
+        } else {
+          logout_failure();
+        }
+      })
+      .catch(() => {
+        logout_failure();
+      });
   }
 }
 
